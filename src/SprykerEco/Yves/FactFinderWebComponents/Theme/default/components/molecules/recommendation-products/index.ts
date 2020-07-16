db0e2a4b20f07758b7fa727ba653c16f536169b7
maslov <dev.maslov.a@gmail.com>
2020-07-16 16:24:59 +0300

    TE-6081: Introduced webpackChunkName commentary.

--- src/SprykerEco/Yves/FactFinderWebComponents/Theme/default/components/molecules/recommendation-products/index.ts
+++ src/SprykerEco/Yves/FactFinderWebComponents/Theme/default/components/molecules/recommendation-products/index.ts
@@ -1,2 +1,5 @@
 import register from 'ShopUi/app/registry';
-export default register('recommendation-products', () => import(/* webpackMode: "" */'./recommendation-products'));
+export default register('recommendation-products', () => import(
+    /* webpackMode: "lazy" */
+    /* webpackChunkName: "recommendation-products" */
+    './recommendation-products'));
